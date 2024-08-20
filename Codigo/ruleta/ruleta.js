952632fb79fbf2082448030a50225c08663c6808
moreaugusto <102055303+moreaugusto@users.noreply.github.com>
2024-08-20 19:55:41 -0300

    Add files via upload

--- Codigo/ruleta/ruleta.js
+++ Codigo/ruleta/ruleta.js
@@ -1,15 +1,13 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const listanumeros = document.querySelectorAll(".numero");
     const anguloPorNumero = 360 / listanumeros.length;
-    const radio = 700; // Radio del círculo en px
+    const radio = 350; // Radio del círculo en px
     let anguloTotal = 0; // Ángulo de rotación total
 
     // Posiciona los números alrededor del centro de la ruleta
     listanumeros.forEach((numero, i) => {
         const angulo = i * anguloPorNumero;
-        // La transformación mueve el número al borde del círculo y luego lo rota para que el texto esté recto
-        numero.style.transform = `rotate(${angulo}deg) translate(${radio/2}px) rotate(${-angulo}deg)`;
+        numero.style.transform = `rotate(${angulo}deg) translate(${radio}px) rotate(${-angulo}deg)`;
         numero.style.top = '50%'; // Centra verticalmente el contenedor
         numero.style.left = '50%'; // Centra horizontalmente el contenedor
         numero.style.transformOrigin = '0 0'; // Establece el origen de transformación en la esquina superior izquierda
@@ -24,33 +22,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Usa setInterval para llamar a actualizarRotacion cada 16 ms (aproximadamente 60 FPS)
     setInterval(actualizarRotacion, 16); // Ajusta el intervalo para cambiar la velocidad
 });
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const listavacia = document.querySelectorAll(".vacio");
-    const angulo2 = 560 / listavacia.length;
-    const radio2 = 600; // Radio del círculo en px
-    let anguloTotal2 = 0; // Ángulo de rotación total  
-
-    // Posiciona los números alrededor del centro de la ruleta
-    listavacia.forEach((vacio, i) => {
-        const angulo = i * angulo2;
-        // La transformación mueve el número al borde del círculo y luego lo rota para que el texto esté recto
-        vacio.style.transform = `rotate(${angulo2}deg) translate(${radio2/2}px) rotate(${-angulo2}deg)`;
-        vacio.style.top = '50%'; // Centra verticalmente el contenedor
-        vacio.style.left = '50%'; // Centra horizontalmente el contenedor
-        vacio.style.transformOrigin = '0 0'; // Establece el origen de transformación en la esquina superior izquierda
-    });
-
-    // Función para actualizar la rotación de la ruleta
-    function actualizarRotacion2() {
-        anguloTotal2 -= 1; // Incrementa el ángulo para la rotación
-        document.querySelector('.ruleta2').style.transform = `rotate(${-anguloTotal2}deg)`; 
-    }
-
-    // Usa setInterval para llamar a actualizarRotacion cada 16 ms (aproximadamente 60 FPS)
-    setInterval(actualizarRotacion2, 15); // Ajusta el intervalo para cambiar la velocidad
-});
-
-
-
